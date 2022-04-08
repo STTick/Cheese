7ac844c0a90f675da89dbc292c9060b24b236358
민지 <71913069+Minj02@users.noreply.github.com>
2022-04-08 13:41:02 +0900

    [CREATE] Prompt 폰트 추가

--- Cheese/CameraViewController.swift
+++ Cheese/CameraViewController.swift
@@ -7,38 +7,64 @@
 
 import UIKit
 
-class CameraViewController: UIViewController , UICollectionViewDataSource,
-                            UICollectionViewDelegate {
-    @IBOutlet weak var filterCollectionView: UICollectionView!
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-
-        //이미지 카운터 하는 함수
-
-        return 3
-
-    }
+class ViewController: UIViewController, UICollectionViewDelegate {
+    var list = ["1", "2", "3", "4" ,"5", "6", "7", "8", "9", "10"]
     
+    @IBOutlet var collectionView: UICollectionView!
     
-
-    
-
-    //셀 구성하기
-
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! FilterCollectionViewCell
-        cell.backgroundColor = .blue
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-        cell.layer.masksToBounds = true
-        cell.layer.cornerRadius = 50
-
-        return cell
-
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+        
     }
-          override func viewDidLoad() {
-              super.viewDidLoad()
-              filterCollectionView.delegate = self
-              filterCollectionView.dataSource = self
-              // Do any additional setup after loading the view.
-          }
-      }
-  
+}
+    
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return list.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CSCollectionViewCell
+        
+        cell.backgroundColor = .lightGray
+        cell.lbl.text = list[indexPath.row]
+        cell.lbl.backgroundColor = .yellow
+        
+        return cell
+    }
+}
+
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+
+    // 위 아래 간격
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+
+    // 옆 간격
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+
+    // cell 사이즈( 옆 라인을 고려하여 설정 )
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+
+        let width = collectionView.frame.width / 3 - 1 ///  3등분하여 배치, 옆 간격이 1이므로 1을 빼줌
+        print("collectionView width=\(collectionView.frame.width)")
+        print("cell하나당 width=\(width)")
+        print("root view width = \(self.view.frame.width)")
+
+        let size = CGSize(width: width, height: width)
+        return size
+    }
+}
+    
+class CSCollectionViewCell : UICollectionViewCell {
+    
+    @IBOutlet var lbl: UILabel!
+}
